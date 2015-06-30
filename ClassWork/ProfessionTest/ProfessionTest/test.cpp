5af305e5d2cc83b8032cb9f7071535cd1c368738
andrewtroyan <andandrew1333@gmail.com>
2015-06-30 23:08:35 +0300

    test

--- ClassWork/ProfessionTest/ProfessionTest/test.cpp
+++ ClassWork/ProfessionTest/ProfessionTest/test.cpp
@@ -22,13 +22,27 @@ void add_questions() {
 	if (file.is_open()) 
 		throw exception("File \"questions.csv\" is not found.");
 
+	int current_index = 0, test_index;
+
 	string buffer;
 	while (!file.eof()) {
 		getline(file, buffer);
+
 		vector<string> values = parse_csv_line(buffer);
 		auto it = values.begin();
+
+		stringstream ss(*it);
+		ss >> test_index;
+		if (current_index != test_index) {
+			file.close();
+			throw exception("There's a mistake in \"traits.csv\".");
+		}
+
 		test::questions.push_back(question(*(++it)));
+		++current_index;
 	}
+
+	file.close();
 }
 
 void add_answers() {
--- ClassWork/ProfessionTest/ProfessionTest/trait.cpp
+++ ClassWork/ProfessionTest/ProfessionTest/trait.cpp
@@ -3,11 +3,13 @@
 #include <fstream>
 #include <exception>
 #include <string>
+#include <sstream>
 
 using std::ifstream;
 using std::exception;
 using std::string;
 using std::getline;
+using std::stringstream;
 
 vector<trait> trait::traits;
 
@@ -16,11 +18,25 @@ void trait::add_traits() {
 	if (!file.is_open())
 		throw exception("File \"traits.csv\" is not found.");
 
+	int current_index = 0, test_index;
+
 	string buffer;
 	while (!file.eof()) {
 		getline(file, buffer);
+
 		vector<string> values = parse_csv_line(buffer);
 		auto it = values.begin();
+
+		stringstream ss(*it);
+		ss >> test_index;
+		if (current_index != test_index) {
+			file.close();
+			throw exception("There's a mistake in \"traits.csv\".");
+		}
+
 		traits.push_back(trait(*(++it)));
+		++current_index;
 	}
+
+	file.close();
 }
