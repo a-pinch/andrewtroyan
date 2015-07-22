cdc2b054ecec5b7b81885b46ef621dca58c38caf
andrewtroyan <andandrew1333@gmail.com>
2015-07-22 11:54:16 +0300

    practice

--- WinAPI/ClassWork/FirstTry/FirstTry/main.cpp
+++ WinAPI/ClassWork/FirstTry/FirstTry/main.cpp
@@ -17,40 +17,43 @@ TCHAR szClassWindow[] = TEXT(" аркасное приложение");	/* »м€ класса окна */
 map<UINT, EventProc*> eventMap;
 
 RECT winParams;
+HINSTANCE hInstance;
+HWND hWnd, hStatic;
+TCHAR buf[20];
 
 int VerticalMove = 0, HorizontalMove = 0;
 
-//LRESULT CALLBACK KeyPressed(HWND hWnd, UINT uMessage, WPARAM wParam, LPARAM lParam) {
-//	GetWindowRect(hWnd, &winParams);
-//	switch (wParam) {
-//	case VK_UP:
-//		if (winParams.top - 10 > 0) {
-//			winParams.top -= 10;
-//			winParams.bottom -= 10;
-//		}
-//		break;
-//	case VK_DOWN:
-//		if (winParams.bottom + 10 < 1080) {
-//			winParams.top += 10;
-//			winParams.bottom += 10;
-//		}
-//		break;
-//	case VK_LEFT:
-//		if (winParams.left - 10 > 0) {
-//			winParams.left -= 10;
-//			winParams.right -= 10;
-//		}
-//		break;
-//	case VK_RIGHT:
-//		if (winParams.right + 10 < 1920) {
-//			winParams.left += 10;
-//			winParams.right += 10;
-//		}
-//		break;
-//	}
-//	MoveWindow(hWnd, winParams.left, winParams.top, winParams.right - winParams.left, winParams.bottom - winParams.top, TRUE);
-//	return 0;
-//}
+////LRESULT CALLBACK KeyPressed(HWND hWnd, UINT uMessage, WPARAM wParam, LPARAM lParam) {
+////	GetWindowRect(hWnd, &winParams);
+////	switch (wParam) {
+////	case VK_UP:
+////		if (winParams.top - 10 > 0) {
+////			winParams.top -= 10;
+////			winParams.bottom -= 10;
+////		}
+////		break;
+////	case VK_DOWN:
+////		if (winParams.bottom + 10 < 1080) {
+////			winParams.top += 10;
+////			winParams.bottom += 10;
+////		}
+////		break;
+////	case VK_LEFT:
+////		if (winParams.left - 10 > 0) {
+////			winParams.left -= 10;
+////			winParams.right -= 10;
+////		}
+////		break;
+////	case VK_RIGHT:
+////		if (winParams.right + 10 < 1920) {
+////			winParams.left += 10;
+////			winParams.right += 10;
+////		}
+////		break;
+////	}
+////	MoveWindow(hWnd, winParams.left, winParams.top, winParams.right - winParams.left, winParams.bottom - winParams.top, TRUE);
+////	return 0;
+////}
 
 LRESULT CALLBACK KeyPressed(HWND hWnd, UINT uMessage, WPARAM wParam, LPARAM lParam) {
 	switch (wParam) {
@@ -74,37 +77,37 @@ LRESULT CALLBACK KeyPressed(HWND hWnd, UINT uMessage, WPARAM wParam, LPARAM lPar
 	return 0;
 }
 
-LRESULT CALLBACK TimerClicked(HWND hWnd, UINT uMessage, WPARAM wParam, LPARAM lParam) {
-	if (VerticalMove || HorizontalMove) {
-		GetWindowRect(hWnd, &winParams);
-		if (VerticalMove > 0) {
-			if (winParams.top - 10 > 0) {
-				winParams.top -= 10;
-				winParams.bottom -= 10;
-			}
-		}
-		else if (VerticalMove < 0) {
-			if (winParams.bottom + 10 < GetSystemMetrics(SM_CYSCREEN)) {
-				winParams.top += 10;
-				winParams.bottom += 10;
-			}
-		}
-		else if (HorizontalMove < 0) {
-			if (winParams.left - 10 > 0) {
-				winParams.left -= 10;
-				winParams.right -= 10;
-			}
-		}
-		else if (HorizontalMove > 0) {
-			if (winParams.right + 10 < GetSystemMetrics(SM_CXSCREEN)) {
-				winParams.left += 10;
-				winParams.right += 10;
-			}
-		}
-		MoveWindow(hWnd, winParams.left, winParams.top, winParams.right - winParams.left, winParams.bottom - winParams.top, TRUE);
-	}
-	return 0;
-}
+//LRESULT CALLBACK TimerClicked(HWND hWnd, UINT uMessage, WPARAM wParam, LPARAM lParam) {
+//	if (VerticalMove || HorizontalMove) {
+//		GetWindowRect(hWnd, &winParams);
+//		if (VerticalMove > 0) {
+//			if (winParams.top - 10 > 0) {
+//				winParams.top -= 10;
+//				winParams.bottom -= 10;
+//			}
+//		}
+//		else if (VerticalMove < 0) {
+//			if (winParams.bottom + 10 < GetSystemMetrics(SM_CYSCREEN)) {
+//				winParams.top += 10;
+//				winParams.bottom += 10;
+//			}
+//		}
+//		else if (HorizontalMove < 0) {
+//			if (winParams.left - 10 > 0) {
+//				winParams.left -= 10;
+//				winParams.right -= 10;
+//			}
+//		}
+//		else if (HorizontalMove > 0) {
+//			if (winParams.right + 10 < GetSystemMetrics(SM_CXSCREEN)) {
+//				winParams.left += 10;
+//				winParams.right += 10;
+//			}
+//		}
+//		MoveWindow(hWnd, winParams.left, winParams.top, winParams.right - winParams.left, winParams.bottom - winParams.top, TRUE);
+//	}
+//	return 0;
+//}
 
 LRESULT CALLBACK ToClose(HWND hWnd, UINT uMessage, WPARAM wParam, LPARAM lParam) {
 	int res;
@@ -139,11 +142,19 @@ LRESULT CALLBACK ToRunAway(HWND hWnd, UINT uMessage, WPARAM wParam, LPARAM lPara
 	return 0;
 }
 
+LRESULT CALLBACK IfLeftButtonClicked(HWND hWnd, UINT uMessage, WPARAM wParam, LPARAM lParam) {
+	hStatic = CreateWindowEx(0, TEXT("STATIC"), 0, WS_CHILD | WS_VISIBLE | WS_BORDER | SS_CENTER | WS_EX_CLIENTEDGE,
+		GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam), 100, 20, hWnd, 0, hInstance, 0);
+	swprintf(buf, 20, TEXT("X: %d, Y: %d"), GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
+	SetWindowText(hStatic, buf);
+	return 0;
+}
+
 INT WINAPI _tWinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPTSTR lpszCmdLine, int nCmdShow)
 {
-	HWND hWnd;
 	MSG Msg;
 	WNDCLASSEX wcl;
+	hInstance = hInst;
 
 	// 1. ќпределение класса окна
 	wcl.cbSize = sizeof(wcl);	// размер структуры WNDCLASSEX
@@ -184,13 +195,14 @@ INT WINAPI _tWinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPTSTR lpszCmdLine, i
 	// 4. ќтображение окна
 	ShowWindow(hWnd, nCmdShow);
 	UpdateWindow(hWnd); // перерисовка окна
-	SetTimer(hWnd, 1, 10, NULL);
+	//SetTimer(hWnd, 1, 10, NULL);
 
-	eventMap[WM_TIMER] = TimerClicked;
+	//eventMap[WM_TIMER] = TimerClicked;
 	eventMap[WM_KEYDOWN] = KeyPressed;
 	eventMap[WM_CLOSE] = ToClose;
 	eventMap[WM_DESTROY] = ToDestroy;
-	eventMap[WM_MOUSEMOVE] = ToRunAway;
+	//eventMap[WM_MOUSEMOVE] = ToRunAway;
+	eventMap[WM_LBUTTONDOWN] = IfLeftButtonClicked;
 
 	// 5. «апуск цикла обработки сообщений
 	while (GetMessage(&Msg, NULL, 0, 0)) // получение очередного сообщени€ из очереди сообщений
---
+++ WinAPI/ClassWork/Tanks/Win32Lesson1/MainCycle.cpp
@@ -0,0 +1,330 @@
+// Win32Lesson1.cpp : Defines the entry point for the application.
+//
+
+#include "stdafx.h"
+#include "Win32Lesson1.h"
+#include <ctime>
+
+#define MAX_LOADSTRING 100
+
+
+// Global Variables:
+
+int robots_count = 0;
+
+HINSTANCE hInst;								// current instance
+TCHAR szTitle[MAX_LOADSTRING];					// The title bar text
+TCHAR szWindowClass[MAX_LOADSTRING];			// the main window class name
+
+// Forward declarations of functions included in this code module:
+ATOM				MyRegisterClass(HINSTANCE hInstance);
+BOOL				InitInstance(HINSTANCE, int);
+LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
+
+void initRobots() {
+	srand(time(NULL));
+
+	//InterfaceRobort* temp_robot = ;
+	Circle::objectStorage.push_back(new Robot(50, 450, "q"));
+	robots_count++;
+	
+	for (auto obj : Circle::objectStorage)
+	{
+		if (dynamic_cast<SuslikiRobot*>(obj) != nullptr)
+			obj->moveToPoint(rand() % 450 + 20, rand() % 450 + 20);
+	}
+}
+
+
+
+int APIENTRY _tWinMain(HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPTSTR    lpCmdLine,
+	int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	// TODO: Place code here.
+	MSG msg;
+	HACCEL hAccelTable;
+
+	// Initialize global strings
+	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadString(hInstance, IDC_WIN32LESSON1, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
+
+	// Perform application initialization:
+	if (!InitInstance(hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+
+	initRobots();
+
+	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WIN32LESSON1));
+
+	// Main message loop:
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	return (int)msg.wParam;
+}
+
+
+
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+//  COMMENTS:
+//
+//    This function and its usage are only necessary if you want this code
+//    to be compatible with Win32 systems prior to the 'RegisterClassEx'
+//    function that was added to Windows 95. It is important to call this function
+//    so that the application will get 'well formed' small icons associated
+//    with it.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WIN32LESSON1));
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = MAKEINTRESOURCE(IDC_WIN32LESSON1);
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassEx(&wcex);
+}
+
+//
+//   FUNCTION: InitInstance(HINSTANCE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+	HWND hWnd;
+
+	hInst = hInstance; // Store instance handle in our global variable
+
+	hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+
+	if (!hWnd)
+	{
+		return FALSE;
+	}
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	COLORREF qRobotColor = RGB(0, 200, 200);
+	line::pen1 = CreatePen(PS_SOLID, 5, qRobotColor);
+
+	COLORREF qDethColor = RGB(255, 140, 0);
+	line::pen2 = CreatePen(PS_SOLID, 3, qDethColor);
+
+	COLORREF qLifeColor = RGB(50, 205, 50);
+	line::pen3 = CreatePen(PS_SOLID, 3, qLifeColor);
+
+	COLORREF qEnemyColor = RGB(200, 20, 133);
+	line::pen4 = CreatePen(PS_SOLID, 5, qEnemyColor);
+
+	// ��������� ��� � 50 ���������� 
+	SetTimer(hWnd, 0, 1000/FPS, 0); // period of refresh screen
+
+	return TRUE;
+}
+
+
+void mainloop(HDC hdc){
+	line::SetHdc(hdc);
+
+	for (auto c : Circle::objectStorage)
+	{
+		// prev step robot or bolt
+		//prev.push_back(new Value(c->x, c->y, c->vx, c->vy, c->id));
+
+		// if robot
+		InterfaceRobort* r = dynamic_cast<InterfaceRobort*>(c);
+		if (r != nullptr) {
+			r->move(robot_speed / FPS);
+		}
+		else {
+			c->move(bolt_speed / FPS);
+		}
+
+		if (c->x > arenaXSize || c->y > arenaYSize || c->x < 0 || c->y < 0)
+			c->energy = 0;
+	}
+
+	// checks cycles 
+	/*
+	// 1. exit from arena size - delete from objectStorage
+	std::vector<std::list<Circle*>::iterator> temp;
+	for (auto os = Circle::objectStorage.begin(); os != Circle::objectStorage.end(); os++)
+	{
+	if ((*os)->v == robot_speed)
+	{
+	if ((*os)->x + robot_radius >= arenaXSize || (*os)->y + robot_radius >= arenaYSize || (*os)->x - robot_radius <= 0 || (*os)->y - robot_radius <= 0)
+	{
+	temp.push_back(os);
+	//Circle::objectStorage.erase(os);
+	robots_count--;
+	}
+	}
+	else
+	{
+	if ((*os)->x >= arenaXSize || (*os)->y >= arenaYSize || (*os)->x <= 0 || (*os)->y <= 0)
+	temp.push_back(os);
+	//Circle::objectStorage.erase(os);
+	}
+	}
+
+	for(auto t: temp)
+	Circle::objectStorage.erase(t);
+
+
+	// 2, 3. bolt hit in robot (vector), taran
+	std::vector<std::list<Circle*>::iterator> dead;
+	for (auto explosion = Circle::objectStorage.begin(); explosion != Circle::objectStorage.end(); explosion++)// Берем текущий
+	{
+	for (auto crexplosion = Circle::objectStorage.crbegin(); crexplosion != Circle::objectStorage.crend(); crexplosion++) // Берем с конца и на начало
+	{
+	if ((*explosion)->x + robot_radius >= (*crexplosion)->x || (*explosion)->y + robot_radius >= (*crexplosion)->y || (*explosion)->x <= (*crexplosion)->x + robot_radius || (*explosion)->y <= (*crexplosion)->y + robot_radius) // Не могу понять проверки
+	{
+	temp.push_back(explosion);
+	robots_count--;
+	}
+	}
+	}
+
+	for (auto t : dead)
+	Circle::objectStorage.erase(t);
+
+	// end
+
+	*/
+
+	for (auto r1_ : Circle::objectStorage)
+	{
+		InterfaceRobort* r1 = dynamic_cast<InterfaceRobort*> (r1_);
+		if (!r1) 	continue;
+
+		for (auto r2 : Circle::objectStorage)
+		{
+			if ((r1_ != r2) && (hypot(r1->x - r2->x, r1->y - r2->y) <= (r1->radius + r2->radius)))
+			{
+				InterfaceRobort* rob2 = dynamic_cast<InterfaceRobort*> (r2);
+				if (rob2)
+				{
+					// robot
+					//pust dynamic _cust to  other type robots 
+					//connect to other Robots (crash robot)
+					int denergy = min(r1->energy, r2->energy);
+					r1->energy -= denergy;
+					r2->energy -= denergy;
+
+				}
+
+				Bolt* bolt = dynamic_cast<Bolt*> (r2);
+				if (bolt != nullptr)
+				{
+					if (bolt->robot != r1)// need all bolt adition link to host Robot
+					{
+						//TODO: debug for add new class AngelRobot
+						r1->energy -= bolt->energy * 3;
+						bolt->energy = 0;
+					}
+				}
+
+			}
+		}
+	}
+
+	//remove_if(objectStorage.begin(), objectStorage.end(), p); crash
+	Circle::objectStorage.erase(remove_if(Circle::objectStorage.begin(), Circle::objectStorage.end(), [](Circle* val)->bool{return val->energy <= 0; }), Circle::objectStorage.end());
+
+	// function() - processing arena after update
+	// if robot is dies - robots_count--
+	// if something change : output to screen!!!!!
+
+	line::indent = 0;
+
+	Rectangle(hdc,0, 0, arenaXSize, arenaYSize);
+
+	for (auto obj : Circle::objectStorage) {
+		obj->Draw(hdc);
+	}
+	//>>>>>>>>>>>>>>>>>//
+}
+
+	
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	int wmId, wmEvent;
+	PAINTSTRUCT ps;
+
+	switch (message)
+	{
+	case WM_TIMER:
+		InvalidateRgn(hWnd, 0, 1);
+		RedrawWindow(hWnd, 0, 0, 0);// RDW_ERASE;
+		break;
+
+	case WM_PAINT:
+		HDC hdc;
+		hdc = BeginPaint(hWnd, &ps);
+		mainloop(hdc);
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	case WM_MOUSEMOVE:
+		for (auto c : Circle::objectStorage)
+		{
+			InterfaceRobort* r = dynamic_cast<InterfaceRobort*>(c);
+			if (r != nullptr) {
+				r->moveToPoint(GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
+			}
+		}
+		break;
+	case WM_LBUTTONDOWN:
+		for (auto c : Circle::objectStorage)
+		{
+			InterfaceRobort* r = dynamic_cast<InterfaceRobort*>(c);
+			if (r != nullptr) {
+				r->moveToPoint(GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
+				Circle::objectStorage.push_back(new Bolt(r->x, r->y, GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam), 10, r));
+			}
+		}
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
