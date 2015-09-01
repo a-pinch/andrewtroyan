a096a5675d65d9177b60041f9b4d9f6d281c15cc
andrewtroyan <andandrew1333@gmai.com>
2015-09-01 20:41:33 +0400

    ...

--- C#/ClassWork/Collections/Collections/Program.cs
+++ C#/ClassWork/Collections/Collections/Program.cs
@@ -11,11 +11,14 @@ namespace Collections
     {
         static void Main(string[] args)
         {
-            Clip[] clips = new Clip[3] { new Clip("1.avi", 2, 1),
-                new Clip("2.avi", 2, 1), new Clip("3.avi",  2, 1) };
+            Clip[] clips = new Clip[3] { new Clip("1.avi", 30, 5),
+                new Clip("2.avi", 45, 9), new Clip("3.avi",  60, 10) };
 
             AdNetwork ad_network = new AdNetwork(clips, new int[4] { 0, 1, 2, 3 });
             ad_network.startWork();
+
+            Console.WriteLine("All data is registered.");
+            Console.ReadKey();
         }
     }
 }
