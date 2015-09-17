790ed8c8c48965f0475c48afd0339aa2e13a1894
andrewtroyan <andandrew1333@gmai.com>
2015-09-18 01:35:00 +0300

    work with html

---
+++ C#/ClassWork/HTTPrequest/HTTPrequest/HtmlWorkingOnSalary.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace HTTPrequest
+{
+    class HtmlWorkingOnSalary
+    {
+        // non-static fields
+
+        private int indexFromUSDtoBYR, indexFromEURtoBYR;
+
+        // properties
+
+        public int IndexFromUSDtoBYR
+        {
+            get { return indexFromUSDtoBYR; }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("In HtmlWorkingOnSalary.IndexFromUSDtoBYR: wrong value!");
+                }
+                indexFromUSDtoBYR = value;
+            }
+        }
+
+        public int IndexFromEURtoBYR
+        {
+            get { return indexFromEURtoBYR; }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("In HtmlWorkingOnSalary.IndexFromEURtoBYR: wrong value!");
+                }
+                indexFromEURtoBYR = value;
+            }
+        }
+
+        // constructor
+
+        public HtmlWorkingOnSalary(int indexFromUSDtoBYR_, int indexFromEURtoBYR_)
+        {
+            IndexFromUSDtoBYR = indexFromUSDtoBYR_;
+            IndexFromEURtoBYR = indexFromEURtoBYR_;
+        }
+
+        // non-static methods
+
+        public int GetSalaryInBYR(string htmlString)
+        {
+            Regex matching = new Regex(@"<meta itemprop=""salaryCurrency"" content=""");
+            htmlString = matching.Replace(htmlString, "");
+
+            int endOfIndex = htmlString.IndexOf("\"");
+            string indexString = htmlString.Substring(0, endOfIndex);
+
+            matching = new Regex(@"[A-Z]+""><meta itemprop=""baseSalary"" content=""");
+            htmlString = matching.Replace(htmlString, "");
+
+            int endOfSum = htmlString.IndexOf("\"");
+            string sumString = htmlString.Substring(0, endOfSum);
+            int sumInt = Int32.Parse(sumString);
+
+            int resultSum;
+
+            switch (indexString)
+            {
+                case "USD":
+                    resultSum = sumInt * IndexFromUSDtoBYR;
+                    break;
+
+                case "EUR":
+                    resultSum = sumInt * IndexFromEURtoBYR;
+                    break;
+
+                default:
+                    resultSum = sumInt;
+                    break;
+            }
+
+            return resultSum;
+        }
+
+        // static methods
+
+        public static bool IsPageLast(string htmlString)
+        {
+            Regex regex = new Regex(@"<span class=""b-pager__next-text"" data-qa=""pager-next-disabled"">", RegexOptions.RightToLeft);
+
+            return regex.IsMatch(htmlString);
+        }
+    }
+}
--- C#/ClassWork/HTTPrequest/HTTPrequest/Program.cs
+++ C#/ClassWork/HTTPrequest/HTTPrequest/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Net;
+using System.Text.RegularExpressions;
 
 namespace HTTPrequest
 {
@@ -12,14 +13,35 @@ namespace HTTPrequest
     {
         static void Main(string[] args)
         {
-            string url = @"http://jobs.tut.by/search/vacancy?text=&specialization=3&area=1002&salary=&currency_code=BYR&experience=doesNotMatter&order_by=relevance&search_period=30&items_on_page=20&no_magic=true";
+            string uri = @"http://jobs.tut.by/search/vacancy?specialization=1&area=1002&enable_snippets=true&no_magic=true&clusters=true&search_period=30&currency_code=BYR&page=";
+            WebClient client = new WebClient();
+            string page;
 
-            WebRequest request = WebRequest.Create(url);
-            WebResponse response = request.GetResponse();
+            HtmlWorkingOnSalary converter = new HtmlWorkingOnSalary(17720, 19950);
 
-            Console.WriteLine(response.ContentType);
+            Regex regexForSalary = new Regex(@"<meta itemprop=""salaryCurrency"" content=""[A-Z]+""><meta itemprop=""baseSalary"" content=""\d+"">");
+            MatchCollection matchedValuesWithSalaries;
 
-            response.Close();
+            using (StreamWriter file = new StreamWriter("..\\..\\salaries.csv"))
+            {
+                int currentPageIndex = 0;
+                bool isLastPage = false;
+
+                while (isLastPage == false)
+                { 
+                    page = client.DownloadString(uri + currentPageIndex);
+                    matchedValuesWithSalaries = regexForSalary.Matches(page);
+
+                    foreach (Match matchedValue in matchedValuesWithSalaries)
+                    {
+                        file.WriteLine(converter.GetSalaryInBYR(matchedValue.Value));
+                    }
+
+                    isLastPage = HtmlWorkingOnSalary.IsPageLast(page);
+                    ++currentPageIndex;
+                }
+            }
+
             Console.ReadKey();
         }
     }
