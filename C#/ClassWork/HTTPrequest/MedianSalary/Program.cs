670a727863450321b7e7ef5c067864b1067c7476
andrewtroyan <andandrew1333@gmai.com>
2015-09-18 09:54:49 +0300

    median salary

---
+++ C#/ClassWork/HTTPrequest/MedianSalary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedianSalary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WorkingOnMedianSalary salaryWork = new WorkingOnMedianSalary("..\\..\\..\\HTTPrequest\\salaries.csv");
+
+            int medianSalary, dispersion;
+            medianSalary = salaryWork.GetMedianSalary(out dispersion);
+
+            Console.WriteLine("Median salary: {0} +- {1} BYR.", medianSalary, dispersion);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/ClassWork/HTTPrequest/MedianSalary/WorkingOnMedianSalary.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MedianSalary
+{
+    class WorkingOnMedianSalary
+    {
+        // non-static fields
+
+        private string fileName;
+
+        // constructor
+
+        public WorkingOnMedianSalary(string fileName_)
+        {
+            fileName = fileName_;
+        }
+
+        // non-static fields
+
+        public int GetMedianSalary(out int dispersion)
+        {
+            List<int> salaries = new List<int>();
+
+            string buffer;
+
+            using (StreamReader file = new StreamReader(fileName))
+            {
+                while (file.EndOfStream == false)
+                {
+                    buffer = file.ReadLine();
+                    salaries.Add(Int32.Parse(buffer));
+                }
+            }
+
+            if (salaries.Count == 0)
+            {
+                dispersion = 0;
+                return 0;
+            }
+            if (salaries.Count == 1)
+            {
+                dispersion = 0;
+                return salaries[0];
+            }
+            else
+            {
+                int median = salaries[salaries.Count / 2];
+                int firstQuarter = salaries[salaries.Count / 4];
+                int lastQuarter = salaries[salaries.Count * 3 / 4];
+
+                dispersion = ((median - firstQuarter) + (lastQuarter - median)) / 2;
+                return median;
+            } 
+        }
+    }
+}
