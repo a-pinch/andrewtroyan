62ba91c70eb3f0cb329507f5f1525ba32f89208b
unknown <st@A2-16.itstep.lan>
2015-09-23 10:44:41 +0300

    serialization

---
+++ C#/ClassWork/Serialization/Serialization/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace Serialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Schooler schoolerIrina = new Schooler("Irina", new int[] { 9, 10, 8, 9 }, new string[] { "pen", "pensil" });
+            Schooler deserializedSchooler;
+
+            #region XML serialization
+
+            //var xmlSerializator = new XmlSerializer(typeof(Schooler));
+
+            //using (var toXml = new StreamWriter(@"..\..\testWithSchoolersXML.xml"))
+            //{
+            //    xmlSerializator.Serialize(toXml, schoolerIrina);
+            //}
+
+            //using (var fromXML = new StreamReader(@"..\..\testWithSchoolersXML.xml"))
+            //{
+            //    deserializedSchooler = (Schooler)xmlSerializator.Deserialize(fromXML);
+            //}
+
+            //Console.WriteLine("Name: {0}.\nMarks: {1}.\nPen case: {2}.", deserializedSchooler._name, String.Join(", ", deserializedSchooler._marks)
+            //    , String.Join(", ", deserializedSchooler._penCase));
+
+
+            #endregion
+
+            #region Binary serialization
+
+            BinaryFormatter binarySerializer = new BinaryFormatter();
+
+            using (var toBIN = File.Create(@"..\..\testWithSchoolersXML.bin"))
+            {
+                binarySerializer.Serialize(toBIN, schoolerIrina);
+            }
+
+            using (var fromBIN = File.OpenRead(@"..\..\testWithSchoolersXML.bin"))
+            {
+                deserializedSchooler = (Schooler)binarySerializer.Deserialize(fromBIN);
+            }
+
+            Console.WriteLine("Name: {0}.\nMarks: {1}.\nPen case: {2}.", deserializedSchooler._name, String.Join(", ", deserializedSchooler._marks)
+                , String.Join(", ", deserializedSchooler._penCase));
+
+            #endregion 
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/ClassWork/Serialization/Serialization/Schooler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+    [Serializable]
+    public class Schooler
+    {
+        // non-static fields
+
+        public string _name;
+        public int[] _marks;
+        public string[] _penCase;
+
+        // constructors
+
+        public Schooler() 
+        {
+            _name = null;
+            _marks = null;
+            _penCase = null;
+        }
+
+        public Schooler(string name, int[] marks, string[] penCase)
+        {
+            _name = name;
+            _marks = marks;
+            _penCase = penCase;
+        }
+    }
+}
