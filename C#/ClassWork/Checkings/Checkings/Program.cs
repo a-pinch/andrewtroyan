327c770792ba10b562f1b1115cb2764529eecfb9
andrewtroyan <andandrew1333@gmail.com>
2015-10-06 09:58:21 +0300

    primes in c#

---
+++ C#/ClassWork/Checkings/Checkings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Checkings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var before = DateTime.Now;
+            WorkWithPrimes.GeneratePrimes(500000, "..\\..\\test.txt");
+            var after = DateTime.Now;
+
+            using (var file = new StreamWriter("..\\..\\result.txt"))
+            {
+                file.WriteLine(after.Subtract(before).TotalSeconds);
+            }
+        }
+    }
+}
---
+++ C#/ClassWork/Checkings/Checkings/WorkWithPrimes.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Checkings
+{
+    class WorkWithPrimes
+    {
+        public static void GeneratePrimes(uint amount, string path)
+        {
+            using (var file = new StreamWriter(path))
+            {
+                if (amount > 0)
+                {
+                    uint count = 0;
+                    int currentNumber = 2;
+
+                    file.WriteLine(currentNumber++);
+                    ++count;
+
+                    while (count < amount)
+                    {
+                        if (CheckForPrimality(currentNumber))
+                        {
+                            file.WriteLine(currentNumber);
+                            ++count;
+                        }
+
+                        currentNumber += 2;
+                    }
+                }
+            }
+        }
+
+        public static bool CheckForPrimality(int number)
+        {
+             for (int i = 2; i * i <= number; i++)
+             {
+                 if (number % i == 0)
+                 {
+                     return false;
+                 }
+             }
+             return true;
+        }
+    }
+}
