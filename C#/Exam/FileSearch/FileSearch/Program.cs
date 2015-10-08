eaa0e823d3ded35c1adfbda2a213269e87dc65aa
andrewtroyan <andandrew1333@gmail.com>
2015-10-08 12:13:44 +0300

    exam

---
+++ C#/Exam/FileSearch/FileSearch/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                WorkWithFile file = new WorkWithFile("..\\..\\text.txt");
+
+                foreach (var word in file.GetMostOccurredWord(60))
+                {
+                    Console.WriteLine(word);
+                }
+            }
+            catch (FileNotFoundException x)
+            {
+                Console.WriteLine(x.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ C#/Exam/FileSearch/FileSearch/WorkWithFile.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileSearch
+{
+    // provides retrieving most occurred words from file
+    class WorkWithFile
+    {
+        // non-static fields
+
+        public string filePath;
+
+        // constructors
+        
+        public WorkWithFile(string filePath_)
+        {
+            filePath = filePath_;
+        }
+
+        // static methods
+
+        // retrieves words from file
+        private static IEnumerable<string> GetWordsFromFile(string filePath)
+        {
+            if (File.Exists(filePath) == false)
+            {
+                throw new FileNotFoundException("In WorkWithFile.GetMostOccurWord(string): file doesn't exist");
+            }
+
+            using (var file = new StreamReader(filePath))
+            {
+                while (file.EndOfStream == false)
+                {
+                    string line = file.ReadLine();
+                    string[] words = line.Split(new char[] {' ', '-', '.', ',', '?', '!', '\'', '\"'}, StringSplitOptions.RemoveEmptyEntries);
+
+                    foreach (var word in words)
+                    {
+                        yield return word.ToLower();
+                    }
+                }
+            }
+        }
+
+        // non-static fields
+
+        // retrieves the given (or less) amount of most occurred words
+        public IEnumerable<string> GetMostOccurredWord(uint amountOfOccurrence)
+        {
+            var result = from word in GetWordsFromFile(filePath)
+                         group word by word into g
+                         orderby g.Count() descending
+                         select g.Key;
+
+            var enumerator = result.GetEnumerator();
+            int counter = 0;
+
+            while (counter++ < amountOfOccurrence && enumerator.MoveNext() == true)
+            {
+                yield return enumerator.Current;
+            }
+        }
+    }
+}
